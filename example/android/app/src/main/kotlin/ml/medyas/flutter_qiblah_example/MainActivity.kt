7c9debbfddd82af998bdfb808c5ec2ecff885d8e
abed-supy-io <abed@supy.io>
2025-10-15 21:52:13 +0400

    update dependencies and configuration for Flutter project; add .gitignore and analysis options

--- example/android/app/src/main/kotlin/ml/medyas/flutter_qiblah_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/ml/medyas/flutter_qiblah_example/MainActivity.kt
@@ -1,7 +1,5 @@
 package ml.medyas.flutter_qiblah_example
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
